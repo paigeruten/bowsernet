f19023d8154ae1be2509f5662e4a9e42957685f8
Paige Ruten <paige.ruten@gmail.com>
2024-12-01 18:38:41 -0600

    Exercise 2-5: Emoji

--- src/browser.rs
+++ src/browser.rs
@@ -1,3 +1,9 @@
+use std::{
+    collections::{HashMap, HashSet},
+    fs::File,
+    io::read_to_string,
+};
+
 use crate::{
     config::{Dimensions, SCROLL_BAR_WIDTH},
     lex, request, ConnectionPool, RequestCache, Url,
@@ -7,11 +13,18 @@ use macroquad::prelude::*;
 const HSTEP: i32 = 15;
 const VSTEP: i32 = 18;
 const SCROLL_STEP: i32 = 100;
+const SUPPORTED_EMOJIS: &str = "\
+    \u{1F600}\u{1F60A}\u{1F60B}\u{1F60D}\u{1F61A}\u{1F61B}\u{1F61C}\u{1F61D}\
+    \u{1F92A}\u{1F600}\u{1F601}\u{1F602}\u{1F603}\u{1F604}\u{1F605}\u{1F606}\
+    \u{1F607}\u{1F609}\u{1F617}\u{1F618}\u{1F619}\u{1F642}\u{1F643}\u{1F923}\
+    \u{1F929}\u{1F970}\u{1F972}\u{1FAE0}\u{263A}";
 
 pub struct Browser {
     connection_pool: ConnectionPool,
     request_cache: RequestCache,
     font: Font,
+    emoji_cache: HashMap<char, Texture2D>,
+    supported_emojis: HashSet<char>,
     display_text: String,
     display_list: Vec<DisplayItem>,
     dimensions: Dimensions,
@@ -25,6 +38,8 @@ impl Browser {
             connection_pool: ConnectionPool::new(),
             request_cache: RequestCache::new(),
             font: load_ttf_font_from_bytes(include_bytes!("../assets/fonts/Times New Roman.ttf"))?,
+            emoji_cache: HashMap::new(),
+            supported_emojis: HashSet::from_iter(SUPPORTED_EMOJIS.chars()),
             display_text: "".to_string(),
             display_list: Vec::new(),
             dimensions: Dimensions {
@@ -58,23 +73,55 @@ impl Browser {
         }
     }
 
-    pub fn draw(&self) {
+    pub fn draw(&mut self) {
         for &DisplayItem { x, y, c } in self.display_list.iter() {
             if y > self.scroll + self.dimensions.height || y + VSTEP < self.scroll {
                 continue;
             }
 
-            draw_text_ex(
-                &c.to_string(),
-                x as f32,
-                (y - self.scroll) as f32,
-                TextParams {
-                    font: Some(&self.font),
-                    font_size: 20,
-                    color: BLACK,
-                    ..Default::default()
-                },
-            );
+            if self.supported_emojis.contains(&c) {
+                let emoji_texture = self.emoji_cache.entry(c).or_insert_with(|| {
+                    let svg = read_to_string(
+                        File::open(format!("assets/emoji/{:X}.svg", c as u32)).unwrap(),
+                    )
+                    .unwrap();
+                    let tree = resvg::usvg::Tree::from_str(
+                        &svg,
+                        &resvg::usvg::Options {
+                            dpi: 96. * 4.,
+                            ..Default::default()
+                        },
+                    )
+                    .unwrap();
+                    let mut pixmap = resvg::tiny_skia::Pixmap::new(72, 72).unwrap();
+                    resvg::render(&tree, Default::default(), &mut pixmap.as_mut());
+                    let png_data = pixmap.encode_png().unwrap();
+                    Texture2D::from_file_with_format(&png_data, Some(ImageFormat::Png))
+                });
+
+                draw_texture_ex(
+                    emoji_texture,
+                    x as f32 - 18.,
+                    (y - self.scroll) as f32 - 24.,
+                    WHITE,
+                    DrawTextureParams {
+                        dest_size: Some(Vec2::new(36., 36.)),
+                        ..Default::default()
+                    },
+                );
+            } else {
+                draw_text_ex(
+                    &c.to_string(),
+                    x as f32,
+                    (y - self.scroll) as f32,
+                    TextParams {
+                        font: Some(&self.font),
+                        font_size: 20,
+                        color: BLACK,
+                        ..Default::default()
+                    },
+                );
+            }
         }
 
         if self.scroll_max > 0 {
